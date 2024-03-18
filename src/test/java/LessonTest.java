c4cd21cd55b195156161f067527a022a95884d9e
Dmitry Varitsky <varitsky@gmail.com>
2024-03-18 18:43:54 +0300

    lesson9 (ДЗ8)

--- src/test/java/LessonTest.java
+++ src/test/java/LessonTest.java
@@ -1,44 +1,32 @@
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 public class LessonTest {
 
   protected static final Logger logger =  LogManager.getLogger(LessonTest.class);
-  static WebDriver driver;
-
-  @BeforeClass
-  public void getDriver() {
-    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromeDriver");
-    driver = new ChromeDriver();
-  }
 
   @AfterClass
-  public static void shutdownDriver() {
-    driver.quit();
-  }
-  @Test(description = "проверяем возможность логина после неверного ввода пароля")
-  public void login() throws InterruptedException {
-    driver.get("https://www.saucedemo.com/");
-    logger.log(Level.INFO, "Вводим данные для входа");
-    driver.findElement(By.cssSelector("input[class='input_error form_input']")).sendKeys("standard_user");
-    driver.findElement(By.cssSelector("input[type='password']")).sendKeys("user_name");
-    driver.findElement(By.cssSelector("input[class='submit-button btn_action']")).click();
-    driver.findElement(By.cssSelector("input[type='password']")).clear();
-    driver.findElement(By.cssSelector("input[type='password']")).sendKeys("secret_sauce");
-    driver.findElement(By.cssSelector("input[class='submit-button btn_action']")).click();
-    Thread.sleep(15000);
+  public static void close() {
+    PageObject.shutdownDriver();
   }
 
-  public void isElementPresent() {
-    assertFalse(driver.findElement(By.cssSelector("input[type='password']")).isDisplayed());
+  @Test(description = "проверяем возможность логина после неверного ввода пароля")
+  public void login() {
+
+    var page = new PageObject();
+    assertTrue(page.isLoginPresent(), "нет div login");
+
+    logger.log(Level.INFO, "Вводим данные для входа и проверяем что после логина появляется app-logo");
+    assertTrue(page.inputLoginName("standard_user")
+        .inputPassword("wrongPassword")
+        .clickLoginButton()
+        .inputPassword("secret_sauce")
+        .clickLoginButton()
+        .isAppLogoPresent());
   }
 }
--- src/test/java/PageObject.java
+++ src/test/java/PageObject.java
@@ -1,12 +1,60 @@
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 
-import java.util.concurrent.TimeUnit;
+public class PageObject {
 
-public class PageObject  {
+  private static WebDriver driver;
+  protected static final Logger logger =  LogManager.getLogger(PageObject.class);
+  private static final By LOGIN_INPUT_FIELD = By.cssSelector("input[type='text']");
+  private static final By PASSWORD_INPUT_FIELD = By.cssSelector("input[type='password']");
+  private static final By LOGIN_BUTTON = By.cssSelector("input[class='submit-button btn_action']");
+  private static final By DIV_ON_LOGIN_PAGE = By.cssSelector("div[class='login_logo']");
+  private static final By DIV_AFTER_LOGIN_PAGE = By.cssSelector("div[class='app_logo']");
+  private static final String url = "https://www.saucedemo.com/";
 
+
+  public PageObject() {
+    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromeDriver");
+    driver = new ChromeDriver();
+    driver.get(url);
+    logger.log(Level.INFO, "переходим на сайт" + url);
+  }
+
+  public PageObject inputLoginName(String login) {
+    logger.log(Level.INFO, "Вводим логин: " + login);
+    driver.findElement(LOGIN_INPUT_FIELD).clear();
+    driver.findElement(LOGIN_INPUT_FIELD).sendKeys(login);
+    return this;
+  }
+
+  public PageObject inputPassword(String password) {
+    logger.log(Level.INFO, "Вводим пароль: " + password);
+    driver.findElement(PASSWORD_INPUT_FIELD).clear();
+    driver.findElement(PASSWORD_INPUT_FIELD).sendKeys(password);
+    return this;
+  }
+
+  public PageObject clickLoginButton() {
+    logger.log(Level.INFO, "Кликаем на кнопку логин");
+    driver.findElement(LOGIN_BUTTON).click();
+    return this;
+  }
+
+  public boolean isLoginPresent() {
+    logger.log(Level.INFO, "Проверяем, что есть элемент login-logo");
+    return driver.findElement(DIV_ON_LOGIN_PAGE).isDisplayed();
+  }
+
+  public boolean isAppLogoPresent() {
+    logger.log(Level.INFO, "Проверяем, что появился элемент app-logo");
+    return driver.findElement(DIV_AFTER_LOGIN_PAGE).isDisplayed();
+  }
+
+  public static void shutdownDriver() {
+    driver.quit();
+  }
 }
